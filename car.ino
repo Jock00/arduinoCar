f33982b25bc5a81347a5a5d5621d399ba34af0d5
Jock00 <44244132+Jock00@users.noreply.github.com>
2019-03-01 22:16:00 +0200

    Create car.ino

---
+++ car.ino
@@ -0,0 +1,117 @@
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+// motor two
+int enB = 5;
+int in3 = 7;
+int in4 = 6;
+void setup()
+{
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(2,OUTPUT);
+  Serial.begin(9600);
+}
+void demoOne()
+{
+  // this function will run the motors in both directions at a fixed speed
+  // turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, 200);
+  // turn on motor B
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, 200);
+  delay(2000);
+  // now change motor directions
+  
+}
+void demoTwo()
+{
+  // this function will run the motors across the range of possible speeds
+  // note that maximum speed is determined by the motor itself and the operating voltage
+  // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
+  // by your hardware
+  // turn on motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH); 
+  analogWrite(enA, 200); 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH); 
+  analogWrite(enB, 200);
+  // accelerate from zero to maximum speed
+
+  // decelerate from maximum speed to zero
+ 
+  // now turn off motors
+  
+}
+void demoThree()
+{
+  // this function will run the motors across the range of possible speeds
+  // note that maximum speed is determined by the motor itself and the operating voltage
+  // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
+  // by your hardware
+  // turn on motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH); 
+  analogWrite(enA, 200); 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW); 
+}
+
+void demoFour()
+{
+  // this function will run the motors across the range of possible speeds
+  // note that maximum speed is determined by the motor itself and the operating voltage
+  // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
+  // by your hardware
+  // turn on motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW); 
+  analogWrite(enB, 200); 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH); 
+}
+
+char data = 0; 
+void loop()
+{
+  if(Serial.available() > 0)      // Send data only when you receive data:
+   {
+      data = Serial.read();        //Read the incoming data & store into data
+      if(data == '1')
+      {
+       
+        demoOne();
+  delay(100);
+      }
+      else if(data =='2')
+      {
+        demoTwo();
+  delay(100);
+
+      }
+      else if(data =='3')
+      {
+        demoThree();
+  delay(100);
+      
+   }
+   else if(data =='4')
+      {
+        demoFour();
+  delay(100);
+  
+  
+}
+   }
+}
